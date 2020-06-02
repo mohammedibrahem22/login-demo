bd4a31448cc8f82be62447df01d16e210ba99b1a
gem -v <gem install --local rubygems-update-2.2.3.gem>
2020-06-02 14:29:20 +0200

    updates to deploy to heroku

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,15 +4,36 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import {FormsModule} from '@angular/forms';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+// import { 
+//   MatToolbarModule, 
+//   MatButtonModule,
+//   MatSidenavModule,
+//   MatIconModule,
+//   MatListModule ,
+//   MatStepperModule,
+//   MatInputModule
+// } from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [LoginComponent, WelcomeComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+		// MatButtonModule,
+		// RouterModule.forChild(routes),
+		MatInputModule,
+		MatFormFieldModule,
+    MatPseudoCheckboxModule,
+    
+		TranslateModule.forRoot(),
   ],
   exports: [LoginComponent]
 })
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
   userName: string;
   password: string;
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+		private translate: TranslateService,
+		private fb: FormBuilder,
+		private cdr: ChangeDetectorRef,
+		private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
@@ -22,5 +29,15 @@ export class LoginComponent implements OnInit {
     }
 
   }
+
+  isControlHasError(controlName: string, validationType: string): boolean {
+		const control = this.loginForm.controls[controlName];
+		if (!control) {
+			return false;
+		}
+
+		const result = control.hasError(validationType) && (control.dirty || control.touched);
+		return result;
+	}
 
 }
